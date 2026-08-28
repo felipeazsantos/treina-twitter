07ee48cad8ac0486950f5ef334b44dbe4138a7b1
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-08-28 13:27:48 -0300

    refactoring index

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,34 +1,12 @@
 import { useState } from 'react'
 import styles from './TextInput.module.css'
 
-export default function TextInput({ placeholder = 'O que está acontecendo?', maxLength, ...props}) {
-    const [text, setText] = useState('');
-    const [tweetList, setTweetList] = useState([])
-
-    function onTextChange(e) {
-        const value = e.target.value;
-        if (value.length <= maxLength) {
-            setText(value)
-        }
-    }
-
-    function sendTweet(e) {
-        setTweetList([...tweetList, text]);
-        setText('')
-    }
-
+export default function TextInput({ ...props}) {
     return (
         <div>
             <textarea 
                 className={styles.input} 
-                placeholder={placeholder} 
-                maxLength={maxLength}
-                value={text}
-                onChange={onTextChange} 
-                {...props} />
-            <p>{text.length} / {maxLength}</p>
-            <button onClick={sendTweet}>Enviar</button>
-            {tweetList.map((tweet, i) => <p key={i}>{tweet}</p>)}
+                {...props} />  
         </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,42 @@
 import TextInput from "../components/TextInput";
+import { useState } from "react";
 
 export default function Index() {
+    const [text, setText] = useState('');
+    const [tweetList, setTweetList] = useState([])
+    const maxLength = 125;
+
+    function onTextChange(e) {
+        const value = e.target.value;
+        if (value.length <= maxLength) {
+            setText(value)
+        }
+    }
+
+    function sendTweet(e) {
+        setTweetList([...tweetList, text]);
+        setText('')
+    }
+
     return (
         <div>
             <h1>TreinaTwitter</h1>
             <div>
                 <img src={'https://github.com/felipeazsantos.png'} alt="" />
-                <TextInput />
+                <TextInput 
+                    placeholder={'O que está acontecendo'}
+                    value={text}
+                    onChange={onTextChange}
+                    maxLength={maxLength} 
+                />
             </div>
             <div>
-                <div>0 / 125</div>
-                <button>Tweetar</button>
+                <div>{text.length} / {maxLength}</div>
+                <button onClick={sendTweet}>Tweetar</button>
             </div>
 
             <ul>
-                <li>Tweet 1</li>
-                <li>Tweet 2</li>
+                  {tweetList.map((tweet, i) => <li key={i}>{tweet}</li>)}
             </ul>
         </div>
     )
